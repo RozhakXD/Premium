3767abb572c93f75401b86cfbabe866b597152a9
RozhakXd <rozaqsapa@gmail.com>
2025-01-01 12:00:00 +0700

    Final Release

---
+++ Prem.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+_A='bold royal_blue1'
+try:import os,urllib.parse;from rich.console import Console;from rich.panel import Panel
+except Exception as error:os.system(f"xdg-open https://wa.me/6283847921480?text=PREMIUM%20%3A%20{urllib.parse.quote(error)}%21");exit()
+os.system('cls'if os.name=='nt'else'clear')
+Console(width=71,style=_A).print(Panel('[bold red]●[bold yellow] ●[bold green] ●\n[bold blue]    ||[bold white]Release :[bold green] 150.0[bold blue]||[bold white]Author :[bold yellow] Rozhak[bold blue]||[bold white]Version :[bold green] 30-Juli-2021[bold blue]||\n[bold red]          ____  ____  ____  __  __  ____  __  __  __  __ \n[bold red]         (  _ \\(  _ \\( ___)(  \\/  )(_  _)(  )(  )(  \\/  )\n[bold red]          )___/ )   / )__)  )    (  _)(_  )(__)(  )    ( \n[bold white]         (__)  (_)\\_)(____)(_/\\/\\_)(____)(______)(_/\\/\\_)\n                 [underline blue]INSTAGRAM BRUTE FORCE PREMIUM'))
+Console(width=71,style=_A).print(Panel(f"""[bold red]Oops! Tampaknya Anda Salah Menjalankan Tools Premium. Perlu Diketah
+ui, Pengembangan Terbaru Untuk Perangkat Selain 64-Bit Belum Bisa Dilakukan Karena Alasan Keamanan Dan Performa Yang Kurang Memadai. Kami Terus Berupaya Mencari Solusi Agar Tools Ini Dapat Digunakan Di Semua Jenis Perangkat.
+
+Jika Anda Ingin Menggunakan Tools Ini, Silakan Gunakan Perintah Berikut:
+[bold yellow]$ cd $HOME/Premium
+$ chmod +x Prem
+$ ./Prem[bold red]
+
+Kami Akan Hadir Dengan Versi Terbaru, Tunggu Update Selanjutnya!""",title='>>> [Catatan] <<<'))
+exit()
